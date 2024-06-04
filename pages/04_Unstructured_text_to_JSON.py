e07c15c7db052f81af0dd91a02cd50f4d2224950
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2024-06-04 17:16:20 +0000

    Fix problems to transform unstructured text into JSON

--- pages/04_Unstructured_text_to_JSON.py
+++ pages/04_Unstructured_text_to_JSON.py
@@ -79,7 +79,7 @@ if not st.session_state.clicked_sec800:
     b = st.button('Generate', key=1, on_click=click_button_1)
 else:
     b = None
-    prompt_gen = "Your task is to take the unstructured text provided and convert it into a well-organized table format using JSON. Identify the main entities, attributes, or categories mentioned in the text and use them as keys in the JSON object. Then, extract the relevant information from the text and populate the corresponding values in the JSON object. Ensure that the data is accurately represented and properly formatted within the JSON structure. The resulting JSON table should provide a clear, structured overview of the information presented in the original text:\n\n"
+    prompt_gen = "Convert this text to JSON format:\n"
     
     if uns_data: 
         prompt_gen += uns_data
@@ -87,25 +87,34 @@ else:
         prompt_gen += uns_text_example
 
     input = {
+        "top_k": 50,
+        "top_p": 0.9,
         "prompt": prompt_gen,
-        "temperature": 0.9
+        "temperature": 0.2,
+        "max_new_tokens": 2048,
+        "stop_sequences": "<|im_end|>",
+        "prompt_template": "<|im_start|>system\nYour task is to take the unstructured text provided and convert it into a well-organized table format using JSON. Identify the main entities, attributes, or categories mentioned in the text and use them as keys in the JSON object. Then, extract the relevant information from the text and populate the corresponding values in the JSON object. Ensure that the data is accurately represented and properly formatted within the JSON structure. The resulting JSON table should provide a clear, structured overview of the information.<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n\n<|im_start|>assistant\n",
     }
     
     prediction_status, prediction_data = generate_llm_data(input)
     
-    if prediction_status == "succeeded": 
+    if prediction_status == "succeeded" and prediction_data:
         response = "".join(prediction_data)
-        json_data = json.loads(response)
-        st.json(json_data, expanded=True)
-        json_string = json.dumps(json_data, indent=2)
-        
-        st.download_button(
-            label="Download JSON",
-            file_name="data.json",
-            mime="application/json",
-            data=json_string,
-        )
-    
+        try:
+            json_data = json.loads(response)
+            st.json(json_data, expanded=True)
+            json_string = json.dumps(json_data, indent=2)
+        except:
+            st.markdown("*The conversion to JSON of the entered text presented some difficulties, it shows the result could be incomplete.*\n\n")
+            st.markdown(response)
+        else:
+            st.download_button(
+                label="Download JSON",
+                file_name="data.json",
+                mime="application/json",
+                data=json_string,
+            )
+
     else:
         st.error("LLM data generation failed. Try again later.")
         st.cache_data.clear()
